d93ea84b5ac7f594863614ffdb5e66e1c2405d6b
Ademar Lisboa <ademar.lisboa@gmail.com>
2020-02-02 17:42:29 -0300

    Inclusão banco H2 e tabela categoria

--- src/main/java/com/curso/cursomc/domain/Categoria.java
+++ src/main/java/com/curso/cursomc/domain/Categoria.java
@@ -2,9 +2,14 @@ package com.curso.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Categoria implements Serializable {
 
-
+	
 	public String getNome() {
 		return nome;
 	}
@@ -14,6 +19,8 @@ public class Categoria implements Serializable {
 	}
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
